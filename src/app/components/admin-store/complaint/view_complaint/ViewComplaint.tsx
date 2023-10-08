e12ac2293e5f91af45ff0101d3db90d296495820
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2023-10-08 21:20:16 +0700

    update(complaint): update list complaint datatable

--- src/app/components/admin-store/complaint/view_complaint/ViewComplaint.tsx
+++ src/app/components/admin-store/complaint/view_complaint/ViewComplaint.tsx
@@ -1,42 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useEffect, useState} from 'react'
 
-import ListComplaintData from '../../../../data/complaint/ViewComplaint'
-
 import './ViewComplaint.css'
 
 import axios from 'axios'
-import Swal from 'sweetalert2'
-import {useNavigate, useParams} from 'react-router-dom'
-import {Table, DatePicker} from 'antd'
+import {useNavigate} from 'react-router-dom'
 import type {ColumnsType} from 'antd/es/table'
 import {Form, InputGroup, Row, Col} from 'react-bootstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBook, faFilter, faPen, faSearch, faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faBook, faFilter, faPen, faSearch} from '@fortawesome/free-solid-svg-icons'
+
+import {Table, DatePicker} from 'antd'
+const {RangePicker} = DatePicker
 
 type Props = {
   className: string
 }
 
 const ViewComplaintStore: React.FC<Props> = ({className}) => {
-  const apiUrl = process.env.REACT_APP_API_URL
   const navigate = useNavigate()
-  const {RangePicker} = DatePicker
 
-  const DateRange = () => {
-    return <RangePicker className='date-range ms-3' />
+  const [dateFrom, setDateFrom] = useState<any>('')
+  const [dateTo, setDateTo] = useState<any>('')
+  const [searchFilter, setSearchFilter] = useState<string>('')
+
+  const handleChangeSearchFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedSearchFilter = event.target.value
+    setSearchFilter(updatedSearchFilter)
   }
 
   interface DataType {
     complaint_id: number
-    // assign_from: string
-    // order_id: number
-    // date_order: string
-    // no_member: string
-    // costumer_name: string
-    // phone_number: number
+    assign_from: string
+    order_id: number
+    date_order: string
+    no_member: string
+    costumer_name: string
+    phone_number: number
     // installer_name: string
-    // order_status: string
+    order_status: string
     // work_status: string
     complaint_date: string
     complaint_desc: string
@@ -49,60 +51,60 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
       dataIndex: 'complaint_id',
       key: 'complaint_id',
       align: 'center',
-      // width: 90,
+      width: 130,
     },
-    // {
-    //   title: 'Assign From',
-    //   dataIndex: 'assign_from',
-    //   key: 'assign_from',
-    //   align: 'center',
-    //   width: 120,
-    // },
-    // {
-    //   title: 'Order ID',
-    //   dataIndex: 'order_id',
-    //   key: 'order_id',
-    //   align: 'center',
-    //   width: 120,
-    // },
-    // {
-    //   title: 'Order Date',
-    //   dataIndex: 'date_order',
-    //   key: 'date_order',
-    //   align: 'center',
-    //   width: 130,
-    // },
-    // {
-    //   title: 'No Member',
-    //   dataIndex: 'no_member',
-    //   key: 'no_member',
-    //   align: 'center',
-    //   width: 130,
-    // },
-    // {
-    //   title: 'Customer Name',
-    //   dataIndex: 'costumer_name',
-    //   key: 'costumer_name',
-    //   width: 150,
-    // },
-    // {
-    //   title: 'No Telp / WA',
-    //   dataIndex: 'phone_number',
-    //   key: 'phone_number',
-    //   width: 160,
-    // },
+    {
+      title: 'Assign From',
+      dataIndex: 'assign_from',
+      key: 'assign_from',
+      align: 'center',
+      width: 120,
+    },
+    {
+      title: 'Order ID',
+      dataIndex: 'order_id',
+      key: 'order_id',
+      align: 'center',
+      width: 120,
+    },
+    {
+      title: 'Order Date',
+      dataIndex: 'date_order',
+      key: 'date_order',
+      align: 'center',
+      width: 130,
+    },
+    {
+      title: 'No Member',
+      dataIndex: 'no_member',
+      key: 'no_member',
+      align: 'center',
+      width: 130,
+    },
+    {
+      title: 'Customer Name',
+      dataIndex: 'costumer_name',
+      key: 'costumer_name',
+      width: 150,
+    },
+    {
+      title: 'No Telp / WA',
+      dataIndex: 'phone_number',
+      key: 'phone_number',
+      width: 160,
+    },
     // {
     //   title: 'Installer Name',
     //   dataIndex: 'installer_name',
     //   key: 'installer_name',
     //   width: 180,
     // },
-    // {
-    //   title: 'Order Status',
-    //   dataIndex: 'order_status',
-    //   key: 'order_status',
-    //   width: 180,
-    // },
+    {
+      title: 'Order Status',
+      dataIndex: 'order_status',
+      key: 'order_status',
+      width: 180,
+    },
     // {
     //   title: 'Work Status',
     //   dataIndex: 'work_status',
@@ -115,21 +117,21 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
       dataIndex: 'complaint_date',
       key: 'complaint_date',
       className: 'col-complaint-date',
-      // width: 90,
+      width: 150,
     },
     {
       title: 'Complaint Description',
       dataIndex: 'complaint_desc',
       key: 'complaint_desc',
       className: 'col-complaint-date',
-      // width: 110,
+      width: 180,
     },
     {
       title: 'Complaint Status',
       dataIndex: 'complaint_status',
       key: 'complaint_status',
       className: 'col-complaint-status',
-      // width: 180,
+      width: 180,
     },
     {
       title: 'Action',
@@ -145,54 +147,6 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
           navigate(`/complaint/update-complaint/${id}`)
         }
 
-        // const handleDelete = () => {
-        //   const id = record.complaint_id
-
-        //   Swal.fire({
-        //     title: 'Are you sure delete this complaint?',
-        //     icon: 'warning',
-        //     showCancelButton: true,
-        //     confirmButtonText: 'Delete Complaint',
-        //     cancelButtonText: 'Cancel',
-        //     reverseButtons: true,
-        //   })
-        //     .then((willDelete) => {
-        //       if (willDelete) {
-        //         axios
-        //           .delete(`${apiUrl}/complaints/${id}`, {
-        //             headers: {
-        //               Accept: 'application/json',
-        //               Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-        //               'Access-Control-Allow-Origin': '*',
-        //               'ngrok-skip-browser-warning': 'true',
-        //             },
-        //           })
-        //           .then((res) => {
-        //             Swal.fire({
-        //               title: 'Success',
-        //               text: res.data.message,
-        //               icon: 'success',
-        //             })
-        //             window.location.reload()
-        //           })
-        //           .catch((error) => {
-        //             Swal.fire({
-        //               title: 'Error',
-        //               text: error.response.data.message,
-        //               icon: 'error',
-        //             })
-        //           })
-        //       }
-        //     })
-        //     .catch((error) => {
-        //       Swal.fire({
-        //         title: 'Error',
-        //         text: error.response.data.message,
-        //         icon: 'error',
-        //       })
-        //     })
-        // }
-
         return (
           <div className='button-wrapper'>
             <a className='button-detail' onClick={handleDetail}>
@@ -202,103 +156,117 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
             <a className='button-edit' onClick={handleEdit}>
               <FontAwesomeIcon icon={faPen} size='sm' />
             </a>
-            {/* 
-            <a className='button-delete' onClick={handleDelete}>
-              <FontAwesomeIcon icon={faTrash} size='sm' />
-            </a> */}
           </div>
         )
       },
       fixed: 'right',
-      width: 50,
+      width: 80,
     },
   ]
-
-  // const data: DataType[] = [
-  //   {
-  //     key: '1',
-  //     complaint_id: '78453992',
-  //     assign_from: 'HO',
-  //     order_id: '78453992',
-  //     date_order: '10/2/2023',
-  //     no_member: '78453992',
-  //     costumer_name: 'Alia',
-  //     phone_number: '08158374638',
-  //     installer_name: 'Water Heater',
-  //     order_status: 'New set up',
-  //     work_status: 'DONE',
-  //     complaint_date: '11/2/2023',
-  //     umur_complaint: '24 jam',
-  //     complaint_status: 'RECEIVED',
-  //   },
-  //   {
-  //     key: '2',
-  //     complaint_id: '78453992',
-  //     assign_from: 'HO',
-  //     order_id: '78453992',
-  //     date_order: '12/2/2023',
-  //     no_member: '78453992',
-  //     costumer_name: 'Abdul',
-  //     phone_number: '08158374638',
-  //     installer_name: 'Water Heater',
-  //     order_status: 'New set up',
-  //     work_status: 'DONE',
-  //     complaint_date: '11/2/2023',
-  //     umur_complaint: '24 jam',
-  //     complaint_status: 'RECEIVED',
-  //   },
-  //   {
-  //     key: '3',
-  //     complaint_id: '78453992',
-  //     assign_from: 'HO',
-  //     order_id: '78453992',
-  //     date_order: '13/2/2023',
-  //     no_member: '78453992',
-  //     costumer_name: 'Ahmad',
-  //     phone_number: '08158374638',
-  //     installer_name: 'AC',
-  //     order_status: 'New set up',
-  //     work_status: 'DONE',
-  //     complaint_date: '11/2/2023',
-  //     umur_complaint: '24 jam',
-  //     complaint_status: 'RECEIVED',
-  //   },
-  //   {
-  //     key: '4',
-  //     complaint_id: '78453992',
-  //     assign_from: 'STORE',
-  //     order_id: '78453992',
-  //     date_order: '15/2/2023',
-  //     no_member: '78453992',
-  //     costumer_name: 'Jean',
-  //     phone_number: '08158374638',
-  //     installer_name: 'Ubin',
-  //     order_status: 'New set up',
-  //     work_status: 'DONE',
-  //     complaint_date: '11/2/2023',
-  //     umur_complaint: '24 jam',
-  //     complaint_status: 'RECEIVED',
-  //   },
-  // ]
 
   // Fetch Data Complaint
   const [complaintData, setComplaintData] = useState<DataType[]>([])
 
+  const formatDate = (date: any) => {
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const year = date.getFullYear()
+    return `${day}/${month}/${year}`
+  }
+
+  const fetchComplaintList = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL
+
+      const response = await axios.get(
+        `${apiUrl}/complaints?date_from=${dateFrom}&date_to=${dateTo}&search=${searchFilter}`,
+        {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+            'Access-Control-Allow-Origin': '*',
+            'ngrok-skip-browser-warning': 'true',
+          },
+        }
+      )
+      return response.data.data
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  const ViewComplaint = async () => {
+    try {
+      const apiData = await fetchComplaintList()
+
+      if (!apiData) {
+        console.error('No data received from fetchOrderList')
+        return []
+      }
+
+      const complaintData = apiData.map((item: any) => {
+        let data
+
+        const orderDate = new Date(item.orders.created_at)
+        const complaintDate = new Date(item.complaint_date)
+
+        let complaintStatus =
+          item.complaint_status === 1
+            ? 'COMPLAINT'
+            : item.complaint_status === 2
+            ? 'INVESTIGATED'
+            : item.complaint_status === 3
+            ? 'ACCEPTED'
+            : ''
+
+        let orderStatus =
+          item.orders.project_status_id === 2
+            ? 'BOOK'
+            : item.orders.project_status_id === 3
+            ? 'BOOKED'
+            : item.orders.project_status_id === 4
+            ? 'SURVEY REQ'
+            : ''
+
+        let phoneNumber =
+          item.orders.members.phone_number !== 'null'
+            ? item.orders.members.phone_number
+            : item.orders.members.whatsapp_number
+
+        data = {
+          complaint_id: item.id,
+          assign_from: item.orders.store.store_name,
+          order_id: item.orders.id,
+          date_order: formatDate(orderDate),
+          no_member: item.orders.members.id,
+          costumer_name: item.orders.members.full_name,
+          phone_number: phoneNumber,
+          // installer_name: item.orders.tukang.full_name,
+          order_status: orderStatus,
+          // work_status: item,
+          complaint_date: formatDate(complaintDate),
+          complaint_desc: item.description,
+          complaint_status: complaintStatus,
+        }
+
+        return data
+      })
+
+      return complaintData
+    } catch (error) {
+      console.error('Error getting order list data:', error)
+      return []
+    }
+  }
+
   useEffect(() => {
     const fetchData = async () => {
-      const data = await ListComplaintData()
+      const data = await ViewComplaint()
       setComplaintData(data)
     }
 
     fetchData()
-  }, [])
-
-  // Search Data Complaint
-  const [searchValue, setSearch] = useState('')
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value)
-  }
+  }, [dateFrom, dateTo, searchFilter])
 
   return (
     <section id='view-complaint'>
@@ -311,7 +279,21 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
                 <h3 className='fs-3 fw-normal'>Date : </h3>
               </div>
 
-              <DateRange />
+              <RangePicker
+                className='date-range ms-3'
+                onChange={(values) => {
+                  if (values && values.length === 2) {
+                    const dateFromFormatted = values[0]?.format('YYYY-MM-DD')
+                    const dateToFormatted = values[1]?.format('YYYY-MM-DD')
+
+                    setDateFrom(dateFromFormatted)
+                    setDateTo(dateToFormatted)
+                  } else {
+                    setDateFrom('')
+                    setDateTo('')
+                  }
+                }}
+              />
             </Col>
 
             <Col xs={12} md={12} lg={12} xl={8} xxl={8}>
@@ -322,10 +304,9 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
                   </InputGroup.Text>
 
                   <Form.Control
-                    value={searchValue}
-                    onChange={handleSearchChange}
                     placeholder='Filter'
                     className='filter-ltr'
+                    onChange={handleChangeSearchFilter}
                   />
                 </InputGroup>
               </div>
@@ -338,7 +319,7 @@ const ViewComplaintStore: React.FC<Props> = ({className}) => {
             columns={columns}
             dataSource={complaintData}
             rowKey={(record) => record.complaint_id}
-            // scroll={{x: 1700}}
+            scroll={{x: 1700}}
             pagination={{position: ['bottomRight']}}
           />
         </div>
