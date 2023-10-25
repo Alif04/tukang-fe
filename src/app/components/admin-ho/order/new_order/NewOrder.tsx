c7c3f3ebf978316f71f4e81c27b2fdaf1ecaf3ef
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2023-10-25 21:54:15 +0700

    update

--- src/app/components/admin-ho/order/new_order/NewOrder.tsx
+++ src/app/components/admin-ho/order/new_order/NewOrder.tsx
@@ -215,7 +215,7 @@ const NewOrderHO: FC = () => {
 
     const getVendor = async () => {
       try {
-        const response = await axios.get(`${apiUrl}/vendor/get`, {
+        const response = await axios.get(`${apiUrl}/vendor`, {
           headers: {
             Accept: 'application/json',
             Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
--- src/app/components/admin-ho/quotation/view_quotation/ViewQuotation.tsx
+++ src/app/components/admin-ho/quotation/view_quotation/ViewQuotation.tsx
@@ -1,287 +1,408 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import './ViewQuotation.css'
 
-import {Table, DatePicker} from 'antd'
+import axios from 'axios'
+import {Table, Tag, DatePicker} from 'antd'
 import type {ColumnsType} from 'antd/es/table'
+import {useNavigate} from 'react-router-dom'
 import {Row, Col, Form, InputGroup} from 'react-bootstrap'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBook, faPen, faTrash, faFilter, faSearch, faPlus} from '@fortawesome/free-solid-svg-icons'
 
-import {useNavigate} from 'react-router-dom'
+const {RangePicker} = DatePicker
 
 type Props = {
   className: string
 }
 
-interface DataType {
-  key: React.Key
-  order_id: string
-  date_order: string
-  product_name: string
-  installation_type: string
-  costumer_id: string
-  costumer_name: string
-  quotation_id: string
-  vendor_name: string
-  amount: string
-  payment_status: string
-  order_status: string
-}
-
-const {RangePicker} = DatePicker
-
-const DateRange = () => {
-  return <RangePicker className='date-range ms-3' />
-}
-
-const DetailButton = () => {
-  const navigate = useNavigate()
-
-  const handleDetail = () => {
-    navigate('/order/detail-order')
-  }
-
-  return (
-    <a className='button-detail' onClick={handleDetail}>
-      <FontAwesomeIcon icon={faBook} size='sm' />
-    </a>
-  )
-}
-
-const EditButton = () => {
-  const navigate = useNavigate()
-
-  const handleEdit = () => {
-    navigate('/order/update-order')
-  }
-
-  return (
-    <a className='button-edit' onClick={handleEdit}>
-      <FontAwesomeIcon icon={faPen} size='sm' />
-    </a>
-  )
-}
-
-const DeleteButton = () => (
-  <a className='button-delete'>
-    <FontAwesomeIcon icon={faTrash} size='sm' />
-  </a>
-)
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: 'Order ID',
-    dataIndex: 'order_id',
-    key: 'order_id',
-    align: 'center',
-    width: 100,
-    className: 'col_order_id',
-  },
-  {
-    title: 'Date Order',
-    dataIndex: 'date_order',
-    key: 'date_order',
-    align: 'center',
-    width: 110,
-  },
-  {
-    title: 'Product Name',
-    dataIndex: 'product_name',
-    key: 'product_name',
-    align: 'left',
-    width: 110,
-  },
-  {
-    title: 'Installation Type',
-    dataIndex: 'installation_type',
-    key: 'installation_type',
-    align: 'left',
-    width: 110,
-  },
-  {
-    title: 'Costumer ID',
-    dataIndex: 'costumer_id',
-    key: 'costumer_id',
-    align: 'center',
-    width: 110,
-  },
-  {
-    title: 'Costumer Name',
-    dataIndex: 'costumer_name',
-    key: 'costumer_name',
-    align: 'left',
-    width: 140,
-  },
-  {
-    title: 'Quotation ID',
-    dataIndex: 'quotation_id',
-    key: 'quotation_id',
-    align: 'center',
-    width: 110,
-  },
-  {
-    title: 'Vendor Name',
-    dataIndex: 'vendor_name',
-    key: 'vendor_name',
-    align: 'left',
-    width: 140,
-  },
-  {
-    title: 'Amount',
-    dataIndex: 'amount',
-    key: 'amount',
-    align: 'left',
-    width: 140,
-  },
-  {
-    title: 'Payment Status',
-    dataIndex: 'payment_status',
-    key: 'payment_status',
-    align: 'left',
-    width: 140,
-  },
-  {
-    title: 'Order Status',
-    dataIndex: 'order_status',
-    key: 'order_status',
-    align: 'left',
-    width: 140,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: () => (
-      <div className='button-wrapper'>
-        <DetailButton />
-        <EditButton />
-        <DeleteButton />
-      </div>
-    ),
-    fixed: 'right',
-    width: 90,
-  },
-]
-
-const data: DataType[] = [
-  {
-    key: '1',
-    order_id: '78453992',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '2',
-    order_id: '78453993',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '3',
-    order_id: '78453994',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '4',
-    order_id: '78453995',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '5',
-    order_id: '78453996',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '6',
-    order_id: '78453997',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-  {
-    key: '7',
-    order_id: '78453998',
-    date_order: '10/2/2023',
-    product_name: 'Water Heater',
-    installation_type: 'New set up',
-    costumer_id: '8986747',
-    costumer_name: 'Alia',
-    quotation_id: '12877450',
-    vendor_name: 'PT.ABC',
-    amount: '500.000',
-    payment_status: 'NONE',
-    order_status: 'QUOTEIN',
-  },
-]
+// const data: DataType[] = [
+//   {
+//     key: '1',
+//     order_id: '78453992',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '2',
+//     order_id: '78453993',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '3',
+//     order_id: '78453994',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '4',
+//     order_id: '78453995',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '5',
+//     order_id: '78453996',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '6',
+//     order_id: '78453997',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+//   {
+//     key: '7',
+//     order_id: '78453998',
+//     date_order: '10/2/2023',
+//     product_name: 'Water Heater',
+//     installation_type: 'New set up',
+//     costumer_id: '8986747',
+//     costumer_name: 'Alia',
+//     quotation_id: '12877450',
+//     vendor_name: 'PT.ABC',
+//     amount: '500.000',
+//     payment_status: 'NONE',
+//     order_status: 'QUOTEIN',
+//   },
+// ]
 
 const ViewQuotationHO: React.FC<Props> = ({className}) => {
+  const navigate = useNavigate()
+
+  const [dateFrom, setDateFrom] = useState<any>('')
+  const [dateTo, setDateTo] = useState<any>('')
+  const [searchFilter, setSearchFilter] = useState<string>('')
+
+  const handleChangeSearchFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedSearchFilter = event.target.value
+    setSearchFilter(updatedSearchFilter)
+  }
+
+  interface DataType {
+    key: React.Key
+    quotation_id: number
+    store_name: string
+    order_id: number
+    date_order: string
+    costumer_name: string
+    vendor_name: string
+    payment_status: string
+    order_status: string
+    quotation_status: string
+  }
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Quotation ID',
+      dataIndex: 'quotation_id',
+      key: 'quotation_id',
+      align: 'center',
+      width: 100,
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.quotation_id - b.quotation_id,
+    },
+    {
+      title: 'Nama Store',
+      dataIndex: 'store_name',
+      key: 'store_name',
+      align: 'center',
+      width: 100,
+      onFilter: (value, record) => record.store_name.includes(String(value)),
+      sorter: (a, b) => a.store_name.length - b.store_name.length,
+    },
+    {
+      title: 'Order ID',
+      dataIndex: 'order_id',
+      key: 'order_id',
+      align: 'center',
+      width: 100,
+      className: 'col_order_id',
+      sorter: (a, b) => a.order_id - b.order_id,
+    },
+    {
+      title: 'Date Order',
+      dataIndex: 'date_order',
+      key: 'date_order',
+      align: 'center',
+      width: 110,
+      onFilter: (value, record) => record.date_order.includes(String(value)),
+      sorter: (a, b) => a.date_order.length - b.date_order.length,
+    },
+    {
+      title: 'Costumer Name',
+      dataIndex: 'costumer_name',
+      key: 'costumer_name',
+      align: 'left',
+      width: 140,
+      onFilter: (value, record) => record.costumer_name.includes(String(value)),
+      sorter: (a, b) => a.costumer_name.length - b.costumer_name.length,
+    },
+    {
+      title: 'Nama Vendor',
+      dataIndex: 'vendor_name',
+      key: 'vendor_name',
+      align: 'left',
+      width: 140,
+      onFilter: (value, record) => record.vendor_name.includes(String(value)),
+      sorter: (a, b) => a.vendor_name.length - b.vendor_name.length,
+    },
+    {
+      title: 'Payment Status',
+      dataIndex: 'payment_status',
+      key: 'payment_status',
+      align: 'left',
+      width: 140,
+      onFilter: (value, record) => record.payment_status.includes(String(value)),
+      sorter: (a, b) => a.payment_status.length - b.payment_status.length,
+    },
+    {
+      title: 'Order Status',
+      dataIndex: 'order_status',
+      key: 'order_status',
+      align: 'left',
+      width: 140,
+      render: (order_status) => {
+        const orderStatus = order_status
+        let color = ''
+
+        switch (orderStatus) {
+          case 'SURVEYDONE':
+            color = 'green'
+            break
+          case 'RESURVEYDONE':
+          default:
+            color = 'blue'
+            break
+        }
+
+        return <Tag color={color}>{orderStatus}</Tag>
+      },
+      filters: [
+        {text: 'BOOK', value: 'BOOK'},
+        {text: 'BOOKED', value: 'BOOKED'},
+      ],
+      onFilter: (value, record) => record.order_status.includes(String(value)),
+      sorter: (a, b) => a.order_status.length - b.order_status.length,
+    },
+    {
+      title: 'Quotation Status',
+      dataIndex: 'quotation_status',
+      key: 'quotation_status',
+      align: 'left',
+      width: 140,
+      render: (quotation_status) => {
+        const orderStatus = quotation_status
+        let color = ''
+
+        switch (orderStatus) {
+          case 'BOOK':
+            color = 'green'
+            break
+          case 'BOOKED':
+            color = 'lime'
+            break
+          case 'SURVEYREQ':
+            color = 'blue'
+            break
+          case 'SURVEYSTART':
+          case 'SURVEYDONE':
+          case 'QUOTE IN':
+          case 'QUOTE OUT':
+          case 'WORKREQ':
+          case 'WORKSTART':
+          case 'WIP':
+          case 'WORKEND':
+          case 'CISOUT':
+            color = 'green'
+            break
+          default:
+            color = 'blue'
+            break
+        }
+
+        return <Tag color={color}>{orderStatus}</Tag>
+      },
+      filters: [
+        {text: 'BOOK', value: 'BOOK'},
+        {text: 'BOOKED', value: 'BOOKED'},
+      ],
+      onFilter: (value, record) => record.quotation_status.includes(String(value)),
+      sorter: (a, b) => a.quotation_status.length - b.quotation_status.length,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      fixed: 'right',
+      width: 90,
+    },
+  ]
+
+  const [orderData, setOrderData] = useState<DataType[]>([])
+
+  const formatDate = (date: any) => {
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = (date.getMonth() + 1).toString().padStart(2, '0')
+    const year = date.getFullYear()
+    return `${day}/${month}/${year}`
+  }
+
+  const fetchOrderList = async () => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL
+
+      const storedStatus = sessionStorage.getItem('statusData')
+      const statusData = storedStatus ? JSON.parse(storedStatus) : []
+
+      const desiredStatusName = 'SURVEYDONE'
+      const desiredStatus = statusData.find((status: any) => status.category === desiredStatusName)
+
+      if (desiredStatus) {
+        const statusId = desiredStatus.value
+
+        const response = await axios.get(
+          `${apiUrl}/orders?date_from=${dateFrom}&date_to=${dateTo}&search=${searchFilter}&take=0&status=${statusId}`,
+          {
+            headers: {
+              Accept: 'application/json',
+              Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+              'Access-Control-Allow-Origin': '*',
+              'ngrok-skip-browser-warning': 'true',
+            },
+          }
+        )
+        return response.data.data
+      } else {
+        console.error('Desired status not found in statusData')
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  const ViewOrder = async () => {
+    try {
+      const apiData = await fetchOrderList()
+
+      if (!apiData) {
+        console.error('No data received from fetchOrderList')
+        return []
+      }
+
+      const orderData = apiData.map((item: any) => {
+        let data
+        const orderDate = new Date(item.created_at)
+
+        let paymentStatus = item.receipt_path === 'null' ? 'UNPAID' : 'PAID'
+
+        data = {
+          quotation_id: item.id,
+          store_name: item.store.store_name,
+          order_id: item.id,
+          date_order: formatDate(orderDate),
+          costumer_name: item.members.full_name,
+          vendor_name: item.vendor.vendor_name,
+          payment_status: paymentStatus,
+          order_status: item.status.category,
+          quotation_status: item.status.category,
+        }
+
+        return data
+      })
+
+      return orderData
+    } catch (error) {
+      console.error('Error getting order list data:', error)
+      return []
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await ViewOrder()
+      setOrderData(data)
+    }
+
+    fetchData()
+  }, [dateFrom, dateTo, searchFilter])
+
   return (
     <section id='view-quotation'>
       <div className={`card ${className}`}>
         <div className='card-body'>
           <Row className='table-head-wrapper'>
             <Col xxl={4} xl={4} lg={4} md={4} sm={12}>
-              <Form.Group as={Row}>
-                <Form.Label className='fs-3' column sm='4'>
-                  <FontAwesomeIcon icon={faFilter} size='sm' className='me-1' />
-                  Date :
-                </Form.Label>
+              <div className='d-flex align-items-center me-3'>
+                <FontAwesomeIcon icon={faFilter} size='2xl' className='me-2' />
+                <h3 className='fs-3 fw-normal'>Date : </h3>
+              </div>
 
-                <Col sm='8'>
-                  <DateRange />
-                </Col>
-              </Form.Group>
+              <RangePicker
+                className='date-range ms-3'
+                onChange={(values) => {
+                  if (values && values.length === 2) {
+                    const dateFromFormatted = values[0]?.format('YYYY-MM-DD')
+                    const dateToFormatted = values[1]?.format('YYYY-MM-DD')
+
+                    setDateFrom(dateFromFormatted)
+                    setDateTo(dateToFormatted)
+                  } else {
+                    setDateFrom('')
+                    setDateTo('')
+                  }
+                }}
+              />
             </Col>
 
             <Col xxl={4} xl={4} lg={4} md={4} sm={12}>
@@ -325,7 +446,7 @@ const ViewQuotationHO: React.FC<Props> = ({className}) => {
             className='table-striped-rows'
             bordered
             columns={columns}
-            dataSource={data}
+            dataSource={orderData}
             rowKey={(record) => record.key}
             scroll={{x: 1800}}
             pagination={{position: ['bottomRight']}}
