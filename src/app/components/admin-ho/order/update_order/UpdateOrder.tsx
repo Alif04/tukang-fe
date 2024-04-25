64daaeeb91e0f6b67b6a343efe1fe7ce826f280d
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-04-26 02:28:24 +0700

    update(app): update feedback

--- src/app/components/admin-ho/order/update_order/UpdateOrder.tsx
+++ src/app/components/admin-ho/order/update_order/UpdateOrder.tsx
@@ -267,12 +267,28 @@ const UpdateOrderHO: FC<{updatePageTitle: (order: Orders) => void}> = ({updatePa
             if (data?.payment_type) {
               if (data.payment_type === 'survey') {
                 setPaymentTypeValue(['berbayar', 'survey'])
+                setOrderForm((prev) => ({
+                  ...prev,
+                  payment_type: 'berbayar',
+                }))
               } else if (data.payment_type === 'gratis') {
                 setPaymentTypeValue(['gratis', 'pemasangan_tanpa_survey'])
+                setOrderForm((prev) => ({
+                  ...prev,
+                  payment_type: 'gratis',
+                }))
               } else if (data.payment_type === 'pemasangan_tanpa_survey') {
                 setPaymentTypeValue(['berbayar', 'pemasangan_tanpa_survey'])
+                setOrderForm((prev) => ({
+                  ...prev,
+                  payment_type: 'pemasangan_tanpa_survey',
+                }))
               } else {
                 setPaymentTypeValue(['gratis', 'pemasangan_tanpa_survey'])
+                setOrderForm((prev) => ({
+                  ...prev,
+                  payment_type: 'gratis',
+                }))
               }
             }
 
@@ -643,6 +659,8 @@ const UpdateOrderHO: FC<{updatePageTitle: (order: Orders) => void}> = ({updatePa
       // ],
     })
   }, [paymentTypeValue])
+
+  console.log('orderForm', orderForm)
 
   // Status
   useEffect(() => {
