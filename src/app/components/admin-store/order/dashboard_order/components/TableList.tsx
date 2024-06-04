702969b88b1d0c42bce1c82b72b3d305d2f8c5c9
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-06-04 11:26:07 +0700

    update(report): update report order

--- src/app/components/admin-store/order/dashboard_order/components/TableList.tsx
+++ src/app/components/admin-store/order/dashboard_order/components/TableList.tsx
@@ -1,16 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 
-import {Table} from 'antd'
+import {Table, PaginationProps} from 'antd'
 import type {ColumnsType} from 'antd/es/table'
 
 type Props = {
   className: string
   orderData: DataType[]
+  currentPage: number
 }
 
 interface DataType {
   order_id: number
+  store_name: string
   costumer_name: string
   service_name: string
   total: string
@@ -25,6 +27,12 @@ const columns: ColumnsType<DataType> = [
     width: 10,
   },
   {
+    title: 'Nama Toko',
+    dataIndex: 'store_name',
+    key: 'store_name',
+    align: 'left',
+  },
+  {
     title: 'Nama Konsumen',
     dataIndex: 'costumer_name',
     key: 'costumer_name',
@@ -32,7 +40,7 @@ const columns: ColumnsType<DataType> = [
     width: 140,
   },
   {
-    title: 'Nama Jasa',
+    title: 'Nama Pemasangan',
     dataIndex: 'service_name',
     key: 'service_name',
     align: 'left',
@@ -47,7 +55,17 @@ const columns: ColumnsType<DataType> = [
   },
 ]
 
-const TableList: React.FC<Props> = ({className, orderData}) => {
+const itemRender: PaginationProps['itemRender'] = (_, type, originalElement) => {
+  if (type === 'prev') {
+    return <a>Prev</a>
+  }
+  if (type === 'next') {
+    return <a>Next</a>
+  }
+  return originalElement
+}
+
+const TableList: React.FC<Props> = ({className, orderData, currentPage}) => {
   return (
     <div className={`card ${className}`}>
       <div className='card-body p-5'>
@@ -59,8 +77,23 @@ const TableList: React.FC<Props> = ({className, orderData}) => {
             columns={columns}
             dataSource={orderData}
             rowKey={(record) => record.order_id}
-            scroll={{x: 1000}}
-            pagination={{position: ['bottomRight']}}
+            scroll={{x: 400, y: 625}}
+            pagination={{
+              current: currentPage,
+              total: orderData?.length ?? 0,
+              showSizeChanger: true,
+              pageSizeOptions: [5, 10, 20, 50, 100],
+              defaultPageSize: 5,
+              // onChange: (page, pageSize) => {
+              //   orderData(page, pageSize)
+              // },
+              itemRender: itemRender,
+              showTotal: (total, range) => (
+                <span style={{left: 0, position: 'absolute'}}>
+                  Showing {range[0]} - {range[1]} of {total} List Order
+                </span>
+              ),
+            }}
           />
         </div>
       </div>
