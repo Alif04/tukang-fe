a798e5ba64057b8e9d7fe76aa88e5dd364815bd3
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-07-23 12:15:47 +0700

    update(notification): update feedback

--- src/app/components/admin-ho/incentive_sales/update_incentive_sales/UpdateIncentiveSales.tsx
+++ src/app/components/admin-ho/incentive_sales/update_incentive_sales/UpdateIncentiveSales.tsx
@@ -301,7 +301,7 @@ const UpdateIncentiveSales: FC = () => {
         },
       })
       .then((response) => {
-        if (response.data.status === 200) {
+        if (response.data.status === 200 || response.data.status === 201) {
           Swal.fire({
             title: 'Success',
             icon: 'success',
