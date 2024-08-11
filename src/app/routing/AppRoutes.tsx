5b97b8ba1bfa33d4a3e21b75564a4f23c990191c
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-08-11 17:01:16 +0700

    update(app): update page route for new url

--- src/app/routing/AppRoutes.tsx
+++ src/app/routing/AppRoutes.tsx
@@ -39,7 +39,7 @@ const AppRoutes: FC = () => {
               <Route path='login' element={<Login />} />
               <Route path='forgot-password' element={<ForgotPassword />} />
               <Route path='reset-password' element={<ResetPassword />} />
-              <Route path='*' element={<Navigate to='/error/404' />} />
+              <Route path='*' element={<Navigate to='/login' />} />
             </>
           ) : (
             <>
