972a03ad7c3a344088275606c8f06db9e8814a80
Ryan Pratama Hidayat <ryan.pratama.h11@gmail.com>
2024-08-29 07:29:48 +0700

    update(app): update feedback

--- src/app/components/admin-ho/reports/daily-followup/Quotation.tsx
+++ src/app/components/admin-ho/reports/daily-followup/Quotation.tsx
@@ -208,35 +208,35 @@ const DailyFollowUpQuotation: React.FC<Props> = ({
       setCurrentPage(response?.data?.page ?? 1)
       setTotalOrder(response?.data?.total ?? 0)
 
-      if (data) {
-        setDailyQuotation((prev: any) => ({
-          ...prev,
-          quotation_follow_up: data?.map((item: any) => ({
-            quotation_id: item?.id ?? null,
-            follow_up_1: item?.quotation_follow_up[0]?.follow_up_1 === true ? 1 : 0,
-            follow_up_2: item?.quotation_follow_up[0]?.follow_up_2 === true ? 1 : 0,
-            follow_up_3: item?.quotation_follow_up[0]?.follow_up_3 === true ? 1 : 0,
-            description: item?.quotation_follow_up[0]?.description ?? '',
-          })),
-        }))
-      }
-
       // if (data) {
       //   setDailyQuotation((prev: any) => ({
       //     ...prev,
-      //     quotation_follow_up: data?.map((item: any) => {
-      //       const lastFollowUp = item?.quotation_follow_up.slice(-1)[0] || {}
-
-      //       return {
-      //         quotation_id: item?.id ?? null,
-      //         follow_up_1: lastFollowUp?.follow_up_1 === true ? 1 : 0,
-      //         follow_up_2: lastFollowUp?.follow_up_2 === true ? 1 : 0,
-      //         follow_up_3: lastFollowUp?.follow_up_3 === true ? 1 : 0,
-      //         description: lastFollowUp?.description ?? '',
-      //       }
-      //     }),
+      //     quotation_follow_up: data?.map((item: any) => ({
+      //       quotation_id: item?.id ?? null,
+      //       follow_up_1: item?.quotation_follow_up[0]?.follow_up_1 === true ? 1 : 0,
+      //       follow_up_2: item?.quotation_follow_up[0]?.follow_up_2 === true ? 1 : 0,
+      //       follow_up_3: item?.quotation_follow_up[0]?.follow_up_3 === true ? 1 : 0,
+      //       description: item?.quotation_follow_up[0]?.description ?? '',
+      //     })),
       //   }))
       // }
+
+      if (data) {
+        setDailyQuotation((prev: any) => ({
+          ...prev,
+          quotation_follow_up: data?.map((item: any) => {
+            const lastFollowUp = item?.quotation_follow_up.slice(-1)[0] || {}
+
+            return {
+              quotation_id: item?.id ?? null,
+              follow_up_1: lastFollowUp?.follow_up_1 === true ? 1 : 0,
+              follow_up_2: lastFollowUp?.follow_up_2 === true ? 1 : 0,
+              follow_up_3: lastFollowUp?.follow_up_3 === true ? 1 : 0,
+              description: lastFollowUp?.description ?? '',
+            }
+          }),
+        }))
+      }
 
       return response.data.data
     } catch (error) {
